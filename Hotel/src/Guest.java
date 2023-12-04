a4057656320978e5ca17a1d5fdfd64e49f1dee8f
SkoupilD <skoupil.d@icloud.com>
2023-12-04 13:48:41 +0100

    Completed program coppied, from local repository

---
+++ Hotel/src/Guest.java
@@ -0,0 +1,43 @@
+import java.time.LocalDate;
+
+public class Guest {
+    String name;
+    String surname;
+    LocalDate dateOfBirth;
+
+    public Guest(String name, String surname, LocalDate dateOfBirth) {
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public String getDescription() {
+        return "Guest: " + name + " " + surname + " (" + dateOfBirth.toString()+")";
+    }
+    public String getFullName(){
+        return  name + " " + surname;
+    }
+}
